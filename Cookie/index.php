347e989915643dbe4a8ce736a241726e6ed82426
Raihan Gafur <raihanthecooldude@gmail.com>
2018-06-25 11:04:01 +0600

    Add files via upload

---
+++ Cookie/index.php
@@ -0,0 +1,36 @@
+<html>
+<body>
+	<form name="LoginPage" method="post" action="LogUser.php">
+		<p>
+			Username : <input type="text" name="un" value=""><br/>
+			Password : <input type="password" name="ps" value=""><br/>
+			<input type="submit" name="Submit"><br/><br/>
+		</p>
+
+<?php
+	//$_SESSION['count']=null;
+	session_start();
+	//$_SESSION['count'];
+	if(!isset($_SESSION['count']))
+	{
+		echo "null";
+	}
+	else
+	{
+		//echo $_SESSION['count'];
+		//var_dump($_S)
+		if($_SESSION['count']=='a')
+		{
+			echo "Invalid username or password\n";
+		}
+		else ($_SESSION['count']=='b')
+		{
+			echo "You have been logged out\n";
+			session_destroy(); //destroy entire session
+		}
+	}
+	
+?>
+	</form>
+</body>
+</html>

ca30e744b46dec909b9e968b07ade4b192fc4ae1
Raihan Gafur <raihanthecooldude@gmail.com>
2018-08-24 15:25:39 +0600

    deleted news

--- news-site-updated-master/login.php
+++
@@ -1,6 +0,0 @@
-<form name="login" action="loginhandler.php" method="POST">
-		User Name:<input type="text" name="un" /><br/>
-		Password:<input type="password" name="pw"/><br/>
-		<button type="submit" name="submit"/>Login</button>
-		<a href="registration.php">Register</a>
-</form>
--- news-site-updated-master/logoutm.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-session_unset($_SESSION['un']);
-session_destroy();
-
-header('location:viewdata.php');
-
-?>

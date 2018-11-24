5a49714084cb9f2d9262e9cf3309f4e4f42ed085
Raihan Gafur <raihanthecooldude@gmail.com>
2018-11-24 08:33:27 +0600

    Ticket Checker C# add

---
+++ Ticket Printer/Ticket Printer/TicketPrinter.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Ticket_Printer
+{
+    partial class TicketPrinter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImagePictureBox = new System.Windows.Forms.PictureBox();
+            this.BrowseBtn = new System.Windows.Forms.Button();
+            this.QuantityLabel = new System.Windows.Forms.Label();
+            this.QuantityTextBox = new System.Windows.Forms.TextBox();
+            this.CreateBtn = new System.Windows.Forms.Button();
+            this.PreviewLabel = new System.Windows.Forms.Label();
+            this.PreviewBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImagePictureBox
+            // 
+            this.ImagePictureBox.BackColor = System.Drawing.Color.White;
+            this.ImagePictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ImagePictureBox.Location = new System.Drawing.Point(12, 30);
+            this.ImagePictureBox.Name = "ImagePictureBox";
+            this.ImagePictureBox.Size = new System.Drawing.Size(426, 134);
+            this.ImagePictureBox.TabIndex = 0;
+            this.ImagePictureBox.TabStop = false;
+            // 
+            // BrowseBtn
+            // 
+            this.BrowseBtn.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BrowseBtn.Location = new System.Drawing.Point(12, 218);
+            this.BrowseBtn.Name = "BrowseBtn";
+            this.BrowseBtn.Size = new System.Drawing.Size(162, 30);
+            this.BrowseBtn.TabIndex = 1;
+            this.BrowseBtn.Text = "Browse";
+            this.BrowseBtn.UseVisualStyleBackColor = true;
+            this.BrowseBtn.Click += new System.EventHandler(this.BrowseClicked);
+            // 
+            // QuantityLabel
+            // 
+            this.QuantityLabel.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuantityLabel.Location = new System.Drawing.Point(12, 357);
+            this.QuantityLabel.Name = "QuantityLabel";
+            this.QuantityLabel.Size = new System.Drawing.Size(89, 24);
+            this.QuantityLabel.TabIndex = 2;
+            this.QuantityLabel.Text = "Quantity:";
+            // 
+            // QuantityTextBox
+            // 
+            this.QuantityTextBox.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuantityTextBox.Location = new System.Drawing.Point(135, 355);
+            this.QuantityTextBox.Name = "QuantityTextBox";
+            this.QuantityTextBox.Size = new System.Drawing.Size(114, 26);
+            this.QuantityTextBox.TabIndex = 3;
+            // 
+            // CreateBtn
+            // 
+            this.CreateBtn.BackColor = System.Drawing.SystemColors.Control;
+            this.CreateBtn.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateBtn.Location = new System.Drawing.Point(16, 438);
+            this.CreateBtn.Name = "CreateBtn";
+            this.CreateBtn.Size = new System.Drawing.Size(162, 30);
+            this.CreateBtn.TabIndex = 4;
+            this.CreateBtn.Text = "Create";
+            this.CreateBtn.UseVisualStyleBackColor = false;
+            this.CreateBtn.Click += new System.EventHandler(this.CreatePDFClicked);
+            // 
+            // PreviewLabel
+            // 
+            this.PreviewLabel.BackColor = System.Drawing.Color.White;
+            this.PreviewLabel.Location = new System.Drawing.Point(600, 30);
+            this.PreviewLabel.Name = "PreviewLabel";
+            this.PreviewLabel.Size = new System.Drawing.Size(310, 438);
+            this.PreviewLabel.TabIndex = 5;
+            // 
+            // PreviewBtn
+            // 
+            this.PreviewBtn.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PreviewBtn.Location = new System.Drawing.Point(303, 351);
+            this.PreviewBtn.Name = "PreviewBtn";
+            this.PreviewBtn.Size = new System.Drawing.Size(162, 30);
+            this.PreviewBtn.TabIndex = 6;
+            this.PreviewBtn.Text = "Preview";
+            this.PreviewBtn.UseVisualStyleBackColor = true;
+            this.PreviewBtn.Click += new System.EventHandler(this.PreviewClicked);
+            // 
+            // TicketPrinter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 633);
+            this.Controls.Add(this.PreviewBtn);
+            this.Controls.Add(this.PreviewLabel);
+            this.Controls.Add(this.CreateBtn);
+            this.Controls.Add(this.QuantityTextBox);
+            this.Controls.Add(this.QuantityLabel);
+            this.Controls.Add(this.BrowseBtn);
+            this.Controls.Add(this.ImagePictureBox);
+            this.Name = "TicketPrinter";
+            this.Text = "Ticket Printer";
+            ((System.ComponentModel.ISupportInitialize)(this.ImagePictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImagePictureBox;
+        private System.Windows.Forms.Button BrowseBtn;
+        private System.Windows.Forms.Label QuantityLabel;
+        private System.Windows.Forms.TextBox QuantityTextBox;
+        private System.Windows.Forms.Button CreateBtn;
+        private System.Windows.Forms.Label PreviewLabel;
+        private System.Windows.Forms.Button PreviewBtn;
+    }
+}
+
---
+++ Ticket Printer/Ticket Printer/TicketPrinter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace Ticket_Printer
+{
+    public partial class TicketPrinter : Form
+    {
+        public TicketPrinter()
+        {
+            InitializeComponent();
+            this.PreviewLabel.Visible = false;
+            this.PreviewBtn.Visible = false;
+        }
+
+        private int quantity = 0;
+
+        OpenFileDialog img = new OpenFileDialog();
+        private void BrowseClicked(object sender, EventArgs e)
+        {
+            img.Filter = "Choose Image(*.jpg;*.png;*.gif)|*.jpg;*.png;*.gif";
+
+            if (img.ShowDialog() == DialogResult.OK)
+            {
+                //ImagePictureBox.Image = Image.FromFile(img.FileName);
+                ImagePictureBox.Image = System.Drawing.Image.FromFile(img.FileName);
+            }
+        }
+
+        private void CreatePDFClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"C:\Users\Asus\Desktop\Ticket" + ".pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+                Document doc = new Document(PageSize.A4);
+
+                PdfWriter writer = PdfWriter.GetInstance(doc, fs);
+                doc.Open();
+
+                //BaseFont bft = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, false);
+                //Font ft = new Font(bft, 12);
+
+                Paragraph p = new Paragraph("\n\n");
+                //doc.Add(p);
+
+                //string url = "C:/Users/Asus/Downloads/atlanta-skyline-by-colton-hd-wallpaper.jpg";
+                string url = img.FileName;
+                iTextSharp.text.Image image = iTextSharp.text.Image.GetInstance(url);
+                //doc.Add(image);
+                quantity = Convert.ToInt32(QuantityTextBox.Text);
+                for (int i = 0; i < quantity; i++)
+                {
+                    doc.Add(image);
+                    doc.Add(p);
+                }
+
+                doc.Close();
+
+                MessageBox.Show("PDF has been generated.", "Done");
+            }
+            catch
+            {
+                MessageBox.Show("There is an error.", "Error");
+            }
+        }
+
+        private void PreviewClicked(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
